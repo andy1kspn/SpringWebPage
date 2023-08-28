29d1d4073a7656d8d5c970d560b4badf3b48019b
andy1kspn <andrei0spinu@gmail.com>
2023-08-27 23:09:04 -0900

    first commit

---
+++ src/main/java/com/okayjava/html/SpinuCorpDev.java
@@ -0,0 +1,13 @@
+package com.okayjava.html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpinuCorpDev {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpinuCorpDev.class, args);
+	}
+
+}
